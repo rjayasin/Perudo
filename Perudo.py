805f99f20072f4bb68441e8401c0a4fff88c535f
rjayasin <rjayasin@users.noreply.github.com>
2014-05-18 12:48:56 -0700

    added mode function, count aces, remove aces

--- Perudo.py
+++ Perudo.py
@@ -1,23 +1,39 @@
 import random
 import probability
 
-players = raw_input("How many players? :")
-timeRolled = int(players)*5
-
 def populateDiceList():
 	dice = []
 	for x in range(int(timeRolled)):
-		gen = random.randrange(1, 500)
+		gen = random.randrange(1, 7)
 		dice.append(gen)
 	return dice
 
+def returnMode(inputList):
+	from collections import Counter
+	data = Counter(inputList)
+	return data.most_common(1)
+
+def countAces(inputList):
+	return inputList.count(1)
+
+def removeAces(inputList):
+	return [value for value in inputList if value != 1]
+
+
+players = raw_input("How many players? :")
+timeRolled = int(players)*5
+
 rolls = populateDiceList();
 print rolls
-this = probability.randomIndex(rolls)
-print "random index " + str(this)
 
+print removeAces(rolls)
+
+
+print countAces(rolls)
+
+# this = probability.randomIndex(rolls)
+# print "random index " + str(this)
 # print len(rolls) , " many rolls"
-
 # print rolls
 # print rolls[0:5]
 # print rolls[5:10]
