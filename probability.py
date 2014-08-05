2afe18bb42e460a33ed02cd0f4c533c15eb99d9a
rjayasin <rjayasin@users.noreply.github.com>
2014-08-05 00:33:54 -0700

    Needed more probability iterations for greater number of players.

--- probability.py
+++ probability.py
@@ -6,13 +6,12 @@ def randomIndex(allRolls):
 	return allRolls[x]
 
 def probOfWin(allRolls, threshold):
-	for x in range (1, 11):
+	for x in range (1, 21):
 		probNow = ((1 - (2.0/3.0)**(len(allRolls)-5))*100)/x
 		probNext = ((1 - (2.0/3.0)**(len(allRolls)-5))*100)/(x + 1)
 		#2/3 probability because 2/6 outcomes = win
 		#Will definitely revise the above probability calculations later. 
 		# print ("The probability of at least " + str(x) 
-				# + " winning die is " + str(probNow) + "%.")
+		# 		+ " winning die is " + str(probNow) + "%.")
 		if threshold > probNext: #this condition doesn't have to be compound!
 			return x
-			
